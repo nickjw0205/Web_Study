34875f56450ad2c06b28c9e208b7ec129642f64e
nomadnick098 <jwsuh0703@gmail.com>
2020-11-16 23:24:22 +0900

    function

---
+++ WEB2_Node.js/study/Array.js
@@ -0,0 +1,18 @@
+//CRUD
+
+var arr = ['A', 'B', 'C', 'D'];
+console.log(arr[1]);
+arr[2] = '3';
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
+
+
+var number = [1,400,21,13,5432];
+var i = 0;
+var  sum = 0;
+while(i < number.length){
+	console.log(number[i]);
+	i = i+1;
+	sum = sum + number[i];
+}
